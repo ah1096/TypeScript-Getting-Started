a1fd8d81376b486cab17aa064c69a22a8d3c2a3f
ah1096 <arh.10.96@gmail.com>
2023-06-14 13:59:17 -0400

    create interfaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 const path = require("path");
 
 module.exports = {
-  entry: './app/app.ts',
-  devtool: 'inline-source-map',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: 'bundle.js'
-  },
+  entry: './ja/app.js',
+  // devtool: 'inline-source-map',
+  // mode: 'development',
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.tsx?$/,
+  //       use: 'ts-loader',
+  //       exclude: /node_modules/
+  //     }
+  //   ]
+  // },
+  // resolve: {
+  //   extensions: [ '.tsx', '.ts', '.js' ]
+  // },
+  // output: {
+  //   filename: 'bundle.js'
+  // },
   devServer: {
     static: path.join(__dirname),
     compress: true,
